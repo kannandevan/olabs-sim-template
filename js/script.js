93ff5aaf38306f36e42c7f789a40dc9648c69762
Devanand <devanand@am.amrita.edu>
2022-05-16 15:26:10 +0530

    index responsive completed login page added

--- js/script.js
+++ js/script.js
@@ -4,5 +4,17 @@ $(document).ready(function () {
         $(".nav").hide();
         $(".nav").fadeIn();
     })
+    $(window).scroll(function() {
+        var $height = $(window).scrollTop();
+      if($height > 50) {
+          $(".hidden-logo").fadeIn(0)
+            $('.hidden-logo').removeClass('d-none');
+            $("nav").addClass("shadow-sm");
+        } else {
+            $(".hidden-logo").fadeOut(0)
+            $('.hidden-logo').addClass('d-none');
+            $("nav").removeClass("shadow-sm");
 
+        }
+    });
 })
