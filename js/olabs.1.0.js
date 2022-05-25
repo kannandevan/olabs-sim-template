eb698a6ab14c6f366d8f2b2386ce6254c32a9f26
Devanand <devanand@am.amrita.edu>
2022-05-25 10:48:55 +0530

    init -some edits

--- js/olabs.1.0.js
+++ js/olabs.1.0.js
@@ -1,26 +1,112 @@
+function Ratio(width, height) {
+    this.width = width;
+    this.height = height;
+    this.length;
+    this.hcf;
+    var lengthW, lengthH
+    this.getHCF = function () {
+        if (this.width % 1 != 0 || this.height % 1 != 0) {
+            if (this.width % 1 != 0 && this.height % 1 != 0) {
+                var ww = this.width.toString().split(".");
+                var hh = this.height.toString().split(".");
+                lengthH = hh[1].length;
+                lengthW = ww[1].length;
+                this.length = this.findBig(lengthH, lengthW);
+                this.width = (Math.pow(10, this.length) * parseInt(ww[0])) + parseInt(ww[1]);
+                this.height = (Math.pow(10, this.length) * parseInt(hh[0])) + parseInt(hh[1]);
+            }
+            else if (this.width % 1 == 0 && this.height % 1 != 0) {
+
+                var hh = this.height.toString().split(".");
+                lengthH = hh[1].length;
+                this.height = (Math.pow(10, lengthH) * parseInt(hh[0])) + parseInt(hh[1]);
+
+                this.width = this.width * Math.pow(10, lengthH);
+
+            }
+            else if (this.width % 1 != 0 && this.height % 1 == 0) {
+
+                var ww = this.width.toString().split(".");
+                lengthW = ww[1].length - 1;
+                this.width = (Math.pow(10, lengthW) * parseInt(ww[0])) + parseInt(ww[1]);
+
+                this.height = this.height * Math.pow(10, lengthW);
+
+            }
+
+
+
+        }
+        for (let i = 1; i <= this.width && i <= this.height; i++) {
+
+            // check if is factor of both integers
+            if (this.width % i == 0 && this.height % i == 0) {
+                this.hcf = i;
+            }
+        }
+        return this.hcf;
+    }
+    this.getAspect = function () {
+        this.hcf = this.getHCF(this.width, this.height);
+        this.ratio = { "widthR": this.width / this.hcf, "heightR": this.height / this.hcf, "hcf": this.hcf }
+
+        return this.ratio
+    }
+    this.findBig = function (a, b) {
+        this.a = a;
+        this.b = b;
+        this.c
+        if (this.a > this.b) {
+            this.b = this.a;
+        } else {
+            this.a = this.b
+        }
+        return this.a;
+    }
+}
 
 /*------------------------------------------------------------------------------------------------
 Component constructor having two variables
 1 - simAreaId : this is the id of thw whole simarea which includes the background of simulation
 2 - componentsClass : this is a common class for all components inside the simulation Area
 ---------------------------------------------------------------------------------------------------*/
-function Component(simAreaId,componentsClass){
+var simContainer;
+function Component(simAreaId, componentsClass) {
     componentsPropraties = [];
     this.simAreaId = simAreaId;
+    simContainer = simAreaId;
+    $(this.simAreaId).height("100vh");
     this.simWidth = $(this.simAreaId).width();
     this.simHeight = $(this.simAreaId).height();
     this.left = $(this.simAreaId).offset().left;
     this.top = $(this.simAreaId).offset().top;
+
+    this.setResponsive = function (componentData) {
+        console.log("hi");
+        this.width = $(this.simAreaId).width();
+        this.height = $(this.simAreaId).height();
+
+        componentData.forEach(element => {
+            var w = $(this.simAreaId).width() * widthP;
+            var h = $(this.simAreaId).height() * heightP;
+            var t = $(element.id).offset().top * topP;
+            var l = $(element.id).offset().left * leftP;
+            $(id).css({ width: w + "px", height: h + "px", top: t + "px", left: l + "px" });
+        });
+
+    }
     $(componentsClass).each(function (index) {
 
-        this.wP = ($(this).width() * 100) / simWidth;
-        this.hP = ($(this).height() * 100) / simHeight;
-        this.lP = ($(this).offset().left * 100) / simWidth;
-        this.tP = ($(this).offset().top * 100) / simHeight;
+
+        this.wP = ($(this).width()) / $(simContainer).width();
+        this.hP = ($(this).height()) / $(simContainer).height();
+
+        this.lP = ($(this).offset().left) / $(simContainer).width();
+        this.tP = ($(this).offset().top) / $(simContainer).height();
 
         var qq = $(this).width();
-        var ww =$(this).height();
-        this.wHRatio = new Ratio($(this).width(),$(this).height()).getAspect();
+        var ww = $(this).height();
+        this.wHRatio = new Ratio($(this).width(), $(this).height()).getAspect();
         this.objData = {
             name: $(this).attr("id"),
             id: "#" + $(this).attr("id"),
@@ -32,10 +118,10 @@ function Component(simAreaId,componentsClass){
             heightP: this.hP,
             leftP: this.lP,
             topP: this.tP,
-            wHRatio : this.wHRatio
+            wHRatio: this.wHRatio
         }
         componentsPropraties.push(this.objData);
     });
 
-    return {simWidth:this.simWidth,simHeight:this.simHeight,componentsPropraties:componentsPropraties};
+    return { simContainerId: this.simAreaId, simWidth: this.simWidth, simHeight: this.simHeight, componentsPropraties: componentsPropraties };
 }
--- js/sim.script.js
+++ js/sim.script.js
@@ -1,1 +1,14 @@
 /* write simulation events here */
+
+
+$(document).ready(function(){
+  var component = new Component("#simContainer",".sim-obj");
+  console.log(component);
+  $(window).resize(function(){
+    component.setResponsive;
+
+  })
+  console.log(component)
+  // component.setResponsive();
+
+})
